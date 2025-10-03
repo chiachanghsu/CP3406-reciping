a74156a50527623a23f8a21f77542cd0cb22399a
xujia <chiachang.hsu@my.jcu.edu.au>
2025-10-04 00:54:59 +0800

    improve with profile page

--- app/src/main/java/com/example/myapp/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/myapp/ui/screens/ProfileScreen.kt
@@ -1,18 +1,46 @@
 package com.example.myapp.ui.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapp.R
 import com.example.myapp.ui.theme.MyAndroidAppTheme
 
 @Composable
 fun ProfileScreen() {
-    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text("Profile page")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.profile), // drawable/profile.(png|jpg|webp)
+            contentDescription = "Profile photo",
+            modifier = Modifier
+                .size(140.dp)
+                .clip(CircleShape)
+                .border(3.dp, MaterialTheme.colorScheme.primary, CircleShape),
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(Modifier.height(16.dp))
+
+        Text(
+            text = "Hsu Chia Chang",
+            style = MaterialTheme.typography.titleMedium
+        )
     }
 }
 
